ec0b21ace1046f65c96297d85eb349a88184fd43
Ryan Howard <ryanghoward@protonmail.com>
2021-09-17 16:57:01 -0700

    Not done but Final Submission

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ function CuboidMaker(attr){
   Formula for cuboid volume: length * width * height   */
 
   CuboidMaker.prototype.volume = function(){
-    return this.length * this.width * this.height;
+    return (this.length * this.width * this.height);
   }
 
 /* 🐴🐴🐴 Step 3: Surface Area Method 🐴🐴🐴
@@ -184,7 +184,7 @@ function CuboidMaker(attr){
   2 * (length * width + length * height + width * height)  */
 
   CuboidMaker.prototype.surfaceArea = function(){
-    return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+    return (2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height)));
   }
 
 /* 🐴🐴🐴 Step 4: Create a new object that uses CuboidMaker 🐴🐴🐴
@@ -196,8 +196,8 @@ function CuboidMaker(attr){
 // 🐴🐴🐴 Test your volume and surfaceArea methods by uncommenting the logs below: 🐴🐴🐴
 // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
 
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()) // 100
+console.log(cuboid.surfaceArea()) // 130
  
 
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
